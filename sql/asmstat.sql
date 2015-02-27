e107f29675601c73dd4d29666a45341260c8c8e9
Takashi Hashizume <th0x4c@gmail.com>
2015-02-27 22:35:35 +0900

    Add a SQL file for ASM.

---
+++ sql/asmstat.sql
@@ -0,0 +1,107 @@
+@@login.sql
+
+/* General */
+@@session.sql
+@@process.sql
+
+/* Concurrency */
+@@latch.sql
+@@latchholder.sql
+@@librarycache.sql
+@@lock.sql
+-- @@locked_object.sql
+@@rowcache.sql
+
+/* Memory */
+@@ksmss.sql
+@@memory_dynamic_components.sql
+@@shared_pool_reserved.sql
+
+/* Wait */
+@@active_session_history_last1.sql
+@@sys_time_model.sql
+@@system_event.sql
+@@system_wait_class.sql
+@@wait_chains.sql
+
+/* RAC */
+@@cr_block_server.sql
+@@ges_blocking_enqueue.sql
+-- @@ges_traffic_controller.sql
+@@resource_limit.sql
+
+/* Stat */
+-- @@archive_dest_status.sql
+@@asm_diskgroup_stat.sql
+@@asm_disk_iostat.sql
+@@asm_disk_stat.sql
+@@asm_operation.sql
+@@asm_volume_stat.sql
+@@buffer_pool_statistics.sql
+-- @@cell_state.sql
+-- @@client_result_cache_stats.sql
+-- @@client_stats.sql
+-- @@cpool_cc_stats.sql
+-- @@cpool_stats.sql
+@@dataguard_stats.sql
+-- @@dataguard_status.sql
+-- @@dnfs_stats.sql
+@@dynamic_remaster_stats.sql
+@@enqueue_stat.sql
+-- @@enqueue_statistics.sql
+-- @@filestat.sql
+-- @@flashback_database_stat.sql
+-- @@fs_failover_stats.sql
+-- @@ges_statistics.sql
+@@hang_statistics.sql
+-- @@iostat_consumer_group.sql
+-- @@iostat_file.sql
+-- @@iostat_function.sql
+@@iostat_function_detail.sql
+@@iostat_network.sql
+-- @@io_calibration_status.sql
+-- @@loadistat.sql
+@@loadpstat.sql
+-- @@lobstat.sql
+-- @@logmnr_stats.sql
+-- @@logstdby_state.sql
+-- @@logstdby_stats.sql
+-- @@mystat.sql
+@@osstat.sql
+@@pgastat.sql
+-- @@pq_sesstat.sql
+@@pq_sysstat.sql
+@@pq_tqstat.sql
+@@px_process_sysstat.sql
+-- @@px_sesstat.sql
+@@qmon_coordinator_stats.sql
+@@qmon_server_stats.sql
+@@qmon_task_stats.sql
+-- @@recovery_file_status.sql
+-- @@recovery_status.sql
+-- @@result_cache_statistics.sql
+@@rman_status.sql
+-- @@rollstat.sql
+-- @@rule_set_aggregate_stats.sql
+-- @@segment_statistics.sql
+-- @@segstat.sql
+-- @@segstat_name.sql
+-- @@service_stats.sql
+-- @@serv_mod_act_stats.sql
+-- @@sesstat.sql
+@@sgastat.sql
+-- @@sqlstats.sql
+-- @@sqlstats_plan_hash.sql
+-- @@sql_cs_statistics.sql
+-- @@sql_plan_statistics.sql
+-- @@sql_plan_statistics_all.sql
+-- @@statistics_level.sql
+-- @@statname.sql
+-- @@streams_pool_statistics.sql
+-- @@subscr_registration_stats.sql
+@@sysstat.sql
+-- @@tempstat.sql
+-- @@undostat.sql
+@@waitstat.sql
+-- @@wlm_pc_stats.sql
+-- @@xstream_table_stats.sql
