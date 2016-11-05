bd651a2b0d30a9382e2845c1b68791fba35c6044
Takashi Hashizume <th0x4c@gmail.com>
2016-11-05 22:37:05 +0900

    Increase linesize for v$session on 12c.

--- sql/login.sql
+++ sql/login.sql
@@ -3,7 +3,7 @@ set term on
 set wrap on
 set trimspool on
 set pagesize 1000
-set linesize 8192
+set linesize 10240
 set numwidth 20
 set echo on
 set tab off
@@ -11,4 +11,3 @@ set tab off
 alter session set nls_date_format = 'YYYY-MM-DD HH24:MI:SS';
 alter session set timed_statistics = true;
 alter session set max_dump_file_size = UNLIMITED;
-
