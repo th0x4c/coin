ccf912d227816dee4c6feda2ad0ce73d4ef63656
Takashi Hashizume <th0x4c@gmail.com>
2016-12-18 02:27:29 +0900

    Debug: wrong AM/PM location.

--- scripts/coin-mpstat.sh
+++ scripts/coin-mpstat.sh
@@ -11,8 +11,8 @@ $COMMAND | awk 'BEGIN{date = strftime("%Y-%m-%d"); \
                       split(strftime("%H:%M:%S"), timestamp, ":");} \
                 /^[012][0-9]:[0-9][0-9]:[0-9][0-9]/{date = strftime("%Y-%m-%d"); \
                                                     split($1, timestamp, ":"); \
-                                                    if ($3 == "PM") {timestamp[1] += 12}; \
-                                                    if ($3 == "AM" && timestamp[1] == 12) {timestamp[1] = 0}} \
+                                                    if ($2 == "PM") {timestamp[1] += 12}; \
+                                                    if ($2 == "AM" && timestamp[1] == 12) {timestamp[1] = 0}} \
                 {printf("'$CNAME' %sT%02d:%02d:%02d %s\n", date, \
                                                            timestamp[1], \
                                                            timestamp[2], \
